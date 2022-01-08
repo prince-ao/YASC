dac8b49b0e6dc72997078809bd65e311f076ae71
prince-ao <princead@tutanota.com>
2022-01-08 12:26:41 -0500

    init

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import Calculator from "./src/Calculator";
+
+export default function App() {
+  return (
+    <Calculator />
+  );
+}
---
+++ src/Calculator.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Calculator = () => {
+  return (
+    <View>
+      <Text>Hello from Calculator</Text>
+    </View>
+  )
+}
+
+export default Calculator
+
+const styles = StyleSheet.create({})
